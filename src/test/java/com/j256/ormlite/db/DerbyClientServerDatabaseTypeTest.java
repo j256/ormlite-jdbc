241891e01e97488d5999da63d2d3e840838a53ed
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-01-26 18:12:12 +0000

    Fleshed out some unit tests for coverage.

--- src/test/java/com/j256/ormlite/db/DerbyClientServerDatabaseTypeTest.java
+++ src/test/java/com/j256/ormlite/db/DerbyClientServerDatabaseTypeTest.java
@@ -1,6 +1,8 @@
 package com.j256.ormlite.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,5 +11,12 @@ public class DerbyClientServerDatabaseTypeTest extends DerbyEmbeddedDatabaseType
 	@Test
 	public void testGetClientServerDriverClassName() {
 		assertEquals("org.apache.derby.jdbc.ClientDriver", new DerbyClientServerDatabaseType().getDriverClassName());
+	}
+
+	@Test
+	public void testIsDatabaseUrlThisType() {
+		assertTrue(new DerbyClientServerDatabaseType().isDatabaseUrlThisType(
+				"jdbc:derby://localhost:1527/MyDbTest;create=true';", "derby"));
+		assertFalse(new DerbyClientServerDatabaseType().isDatabaseUrlThisType("jdbc:derby:database", "derby"));
 	}
 }
