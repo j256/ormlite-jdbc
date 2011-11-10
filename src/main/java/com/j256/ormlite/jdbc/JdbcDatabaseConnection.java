a020a2941cc5f752f406b40134abf37adcd8d550
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-11-10 18:35:53 +0000

    Added Dao.idExists(ID id) to see if row with id exists. Thanks to Craig Andrews. Resolves feature #3300688.

--- src/main/java/com/j256/ormlite/jdbc/JdbcDatabaseConnection.java
+++ src/main/java/com/j256/ormlite/jdbc/JdbcDatabaseConnection.java
@@ -163,8 +163,12 @@ public class JdbcDatabaseConnection implements DatabaseConnection {
 	}
 
 	public long queryForLong(String statement) throws SQLException {
+		return queryForLong(statement, noArgs, noArgTypes);
+	}
+
+	public long queryForLong(String statement, Object[] args, FieldType[] argFieldTypes) throws SQLException {
 		// don't care about the object cache here
-		Object result = queryForOne(statement, noArgs, noArgTypes, longWrapper, null);
+		Object result = queryForOne(statement, args, argFieldTypes, longWrapper, null);
 		if (result == null) {
 			throw new SQLException("No results returned in query-for-long: " + statement);
 		} else if (result == MORE_THAN_ONE) {
