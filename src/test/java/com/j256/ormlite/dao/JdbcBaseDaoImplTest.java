1ab4efa72020959f05f54a4404c50aa92905f587
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2012-04-12 05:44:27 +0000

    Added support for start and endThreadConnection() and added dao.commit() and rollBack().  Thanks to AlexPal.

--- src/test/java/com/j256/ormlite/dao/JdbcBaseDaoImplTest.java
+++ src/test/java/com/j256/ormlite/dao/JdbcBaseDaoImplTest.java
@@ -43,6 +43,7 @@ import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.SelectArg;
 import com.j256.ormlite.stmt.UpdateBuilder;
 import com.j256.ormlite.stmt.Where;
+import com.j256.ormlite.support.DatabaseConnection;
 import com.j256.ormlite.table.DatabaseTable;
 import com.j256.ormlite.table.DatabaseTableConfig;
 import com.j256.ormlite.table.TableUtils;
@@ -3429,6 +3430,69 @@ public class JdbcBaseDaoImplTest extends BaseJdbcTest {
 		assertEquals(2, foo1.version);
 		// but when we try to update the earlier foo, the version doesn't match
 		assertEquals(0, dao.update(foo1));
+	}
+
+	@Test
+	public void testAutoCommitClose() throws Exception {
+		Dao<Foo, Integer> dao = createDao(Foo.class, true);
+		DatabaseConnection conn = null;
+		try {
+			conn = dao.startThreadConnection();
+			dao.setAutoCommit(conn, false);
+
+			Foo foo = new Foo();
+			assertEquals(1, dao.create(foo));
+			List<Foo> results = dao.queryForAll();
+			assertEquals(1, results.size());
+
+			closeConnectionSource();
+			DaoManager.clearCache();
+			openConnectionSource();
+
+			dao = createDao(Foo.class, true);
+
+			results = dao.queryForAll();
+			// we expect there to be no results because we closed the connection to the database before a commit
+			// happened
+			assertEquals(0, results.size());
+		} finally {
+			dao.endThreadConnection(conn);
+		}
+	}
+
+	@Test
+	public void testConnectionMethods() throws Exception {
+		Dao<Foo, Integer> dao = createDao(Foo.class, true);
+		DatabaseConnection conn = null;
+		try {
+			conn = dao.startThreadConnection();
+			assertTrue(dao.isAutoCommit(conn));
+			dao.setAutoCommit(conn, false);
+			assertFalse(dao.isAutoCommit(conn));
+
+			Foo foo = new Foo();
+			assertEquals(1, dao.create(foo));
+			assertNotNull(dao.queryForId(foo.id));
+
+			dao.rollBack(conn);
+			assertNull(dao.queryForId(foo.id));
+
+			foo = new Foo();
+			assertEquals(1, dao.create(foo));
+			assertNotNull(dao.queryForId(foo.id));
+
+			dao.commit(conn);
+			assertNotNull(dao.queryForId(foo.id));
+
+			dao.rollBack(conn);
+			assertNotNull(dao.queryForId(foo.id));
+
+		} finally {
+			if (conn != null) {
+				conn.setAutoCommit(true);
+				dao.endThreadConnection(conn);
+			}
+		}
 	}
 
 	/* ==================================================================================== */
