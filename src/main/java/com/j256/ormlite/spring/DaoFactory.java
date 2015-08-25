53c9681c54654f6c9e4253aa29a25433837a1e62
Noor Dawod <noor@fine47.com>
2015-08-25 17:59:03 +0200

    Just the necessary minimal in order to allow building.

--- src/main/java/com/j256/ormlite/spring/DaoFactory.java
+++ src/main/java/com/j256/ormlite/spring/DaoFactory.java
@@ -11,24 +11,31 @@ import com.j256.ormlite.table.DatabaseTableConfig;
  * <p>
  * Spring bean that can be used to create Dao's of certain classes without needing their own Dao class.
  * </p>
- * 
+ *
  * <p>
  * Here is an example of spring wiring. See the Spring example in the documentation for more info.
  * </p>
- * 
+ *
  * <pre>
  * 	&lt;bean id="accountDao" class="com.j256.ormlite.spring.DaoFactory" factory-method="createDao"&gt;
  * 		&lt;constructor-arg index="0" ref="connectionSource" /&gt;
  * 		&lt;constructor-arg index="1" value="com.j256.ormlite.examples.spring.Account" /&gt;
  * 	&lt;/bean&gt;
  * </pre>
- * 
+ *
  * @author graywatson
  */
 public class DaoFactory {
 
 	/**
 	 * Create and return a Dao based on the arguments.
+	 *
+         * @throws SQLException
+	 * @param <ID>
+	 * @param <T>
+	 * @param connectionSource
+	 * @param clazz
+	 * @return
 	 */
 	public static <T, ID> Dao<T, ID> createDao(ConnectionSource connectionSource, Class<T> clazz) throws SQLException {
 		return DaoManager.createDao(connectionSource, clazz);
@@ -36,6 +43,13 @@ public class DaoFactory {
 
 	/**
 	 * Create and return a Dao based on the arguments.
+	 *
+         * @throws SQLException
+	 * @param <ID>
+	 * @param <T>
+	 * @param connectionSource
+	 * @param tableConfig
+	 * @return
 	 */
 	public static <T, ID> Dao<T, ID> createDao(ConnectionSource connectionSource, DatabaseTableConfig<T> tableConfig)
 			throws SQLException {
