6da06aed7b4204791dd3964c04bc9343bbc21ca8
Gray Watson <gray.github@mailnull.com>
2016-06-01 18:04:36 -0400

    Removed check for Xerial type which is no longer valid.  Thanks yarulan.

--- src/main/java/com/j256/ormlite/db/SqliteDatabaseType.java
+++ src/main/java/com/j256/ormlite/db/SqliteDatabaseType.java
@@ -1,8 +1,5 @@
 package com.j256.ormlite.db;
 
-import com.j256.ormlite.logger.Logger;
-import com.j256.ormlite.logger.LoggerFactory;
-
 /**
  * Sqlite database type information used to create the tables, etc..
  * 
@@ -13,9 +10,6 @@ public class SqliteDatabaseType extends BaseSqliteDatabaseType {
 	private final static String DATABASE_URL_PORTION = "sqlite";
 	private final static String DRIVER_CLASS_NAME = "org.sqlite.JDBC";
 	private final static String DATABASE_NAME = "SQLite";
-	private final static String XERIAL_DRIVER_CLASS = "org.ibex.nestedvm.Interpreter";
-
-	private static final Logger logger = LoggerFactory.getLogger(SqliteDatabaseType.class);
 
 	public SqliteDatabaseType() {
 	}
@@ -27,13 +21,6 @@ public class SqliteDatabaseType extends BaseSqliteDatabaseType {
 
 	@Override
 	protected String getDriverClassName() {
-		try {
-			// make sure we are using the Xerial driver
-			Class.forName(XERIAL_DRIVER_CLASS);
-		} catch (Exception e) {
-			logger.error("WARNING: you seem to not be using the Xerial SQLite driver.  "
-					+ "See ORMLite docs on SQLite: http://ormlite.com/docs/sqlite");
-		}
 		return DRIVER_CLASS_NAME;
 	}
 
