0eb5944254b64a73bb16b0ee095a68e190b63108
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2025-10-10 06:36:39 +0200

    COrrect semicolon placement

--- src/main/java/com/j256/ormlite/jdbc/JdbcDatabaseConnection.java
+++ src/main/java/com/j256/ormlite/jdbc/JdbcDatabaseConnection.java
@@ -129,10 +129,10 @@ public class JdbcDatabaseConnection implements DatabaseConnection {
 		if (resultFlags == DatabaseConnection.DEFAULT_RESULT_FLAGS) {
 			resultFlags = ResultSet.TYPE_FORWARD_ONLY;
 		}
-		try (Statement statement = connection.createStatement(resultFlags, ResultSet.CONCUR_READ_ONLY)) {
+		try (Statement statement = connection.createStatement(resultFlags, ResultSet.CONCUR_READ_ONLY);) {
 			statement.execute(statementStr);
 			return statement.getUpdateCount();
-		};
+		}
 	}
 
 	@Override
